c68193c0ac893e7dc2dff0a5d11f6a13df8ad03d
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-21 13:59:38 +0300

    zero-and-one assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Good Luck 💪🏾
+
+
+
+
+function ZeroAndOne(arr) {
+  let result = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 'Zero') {
+      result += '0';
+    } else if (arr[i] === 'One') {
+      result += '1';
+    }
+  }
+  return result;
+}
+
+
+console.log(ZeroAndOne(["one", "Zero", "Zero", "One"])); 
+console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"])); 
+
+
